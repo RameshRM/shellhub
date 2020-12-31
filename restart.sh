85e8d0d532af3c41dc5eba847958c3bab7fc97ec
Mahadevan,Ramesh(ramahadevan) <ramahadevan@ebay.com>
2020-12-31 10:44:47 -0800

    Updated to start agent

--- restart.sh
+++ restart.sh
@@ -11,20 +11,20 @@
 #  -e SHELLHUB_PRIVATE_KEY=./ssh_private_key \
 #  -e SHELLHUB_TENANT_ID=DC8B5B5D-8CEE-43E1-8FF7-CFA1A780BF84 \
 #  agent:latest
-#docker rm -f shellhub
+docker rm -f shellhub
 
-#docker build -f ./agent/Dockerfile.amd64 ./ -t agent:latest
-
-#docker run \
-#  --network="host" \
-#   -v /var/run/docker.sock:/var/run/docker.sock \
-#  -e SHELLHUB_SERVER_ADDRESS=http://localhost \
-#  -e SHELLHUB_PRIVATE_KEY=./ssh_private_key \
-#  -e SHELLHUB_TENANT_ID=DC8B5B5D-8CEE-43E1-8FF7-CFA1A780BF84 \
-#  agent:latest
+docker build -f ./agent/Dockerfile.amd64 ./ -t agent:latest
 
 docker run \
-    --network="host" \
+  --network="host" \
    -v /var/run/docker.sock:/var/run/docker.sock \
-   -e SHELLHUB_SERVER_ADDRESS=http://localhost:3333 \
+  -e SHELLHUB_SERVER_ADDRESS=http://localhost \
+  -e SHELLHUB_PRIVATE_KEY=./ssh_private_key \
+  -e SHELLHUB_TENANT_ID=DC8B5B5D-8CEE-43E1-8FF7-CFA1A780BF84 \
   agent:latest
+
+#docker run \
+#    --network="host" \
+#   -v /var/run/docker.sock:/var/run/docker.sock \
+#   -e SHELLHUB_SERVER_ADDRESS=http://localhost:3333 \
+#  agent:latest
